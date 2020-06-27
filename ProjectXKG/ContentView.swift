75a71f9b28855bacba6e1144a2ea333de390ebfe
Grzegorz Gumieniak <kontaktg.gumieniak@gmail.com>
2020-06-27 12:51:50 +0200

    Initial Commit

---
+++ ProjectXKG/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ProjectXKG
+//
+//  Created by Grzegorz Gumieniak on 27/06/2020.
+//  Copyright © 2020 Grzegorz Gumieniak. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
