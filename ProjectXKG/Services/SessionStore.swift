a9e7ece2c756b2d9d3dc4d9a8485d7ebe4e38659
Grzegorz Gumieniak <kontaktg.gumieniak@gmail.com>
2020-07-18 10:25:26 +0200

    Dodanie zawartosci klasy do przechowywania sesji

--- ProjectXKG/Services/SessionStore.swift
+++ ProjectXKG/Services/SessionStore.swift
@@ -10,6 +10,46 @@ import SwiftUI
 import Firebase
 import Combine
 
-class SessionStore {
+class SessionStore: ObservableObject {
+    var didChange = PassthroughSubject<SessionStore, Never>()
+    var handle: AuthStateDidChangeListenerHandle?
+    @Published var session: User? {
+        didSet {self.didChange.send(self)}
+    }
+    
+    deinit {
+        unbing()
+    }
+    
+    func listen() {
+        handle = Auth.auth().addStateDidChangeListener({ (auth,user) in
+            if let user = user {
+                self.session = User(uid: user.uid, email: user.email)
+            }
+        })
+    }
+    
+    func signUp(email: String, password: String, handler: @escaping AuthDataResultCallback) {
+        Auth.auth().createUser(withEmail: email, password: password, completion: handler)
+    }
+    
+    func signIn(email: String, password: String, handler: @escaping AuthDataResultCallback) {
+        Auth.auth().signIn(withEmail: email, password: password, completion: handler)
+    }
+    
+    func signOut() {
+        do {
+            try Auth.auth().signOut()
+            self.session = nil
+        } catch {
+            print("Error when signOut \(error.localizedDescription)")
+        }
+    }
+    
+    func unbing() {
+        if let handle = handle {
+            Auth.auth().removeStateDidChangeListener(handle)
+        }
+    }
     
 }
