ca45971e01659433575ec1043d257f7e72781a44
Grzegorz Gumieniak <60929793+ggumieniak@users.noreply.github.com>
2020-09-18 10:17:33 +0200

    Update skrypt.js

--- skrypt.js
+++ skrypt.js
@@ -1,24 +1,49 @@
-// Adapt to project
-// Your web app's Firebase configuration
-var firebaseConfig = {
-      apiKey: "AIzaSyAwg1WkyyTjrAuSm6-oTFEi38hygAVt38I",
-      authDomain: "flash-chat-ios13-f54d0.firebaseapp.com",
-      databaseURL: "https://flash-chat-ios13-f54d0.firebaseio.com",
-      projectId: "flash-chat-ios13-f54d0",
-      storageBucket: "flash-chat-ios13-f54d0.appspot.com",
-      messagingSenderId: "796065256137",
-      appId: "1:796065256137:web:2cfd38a02fceaf2a6bb318"
-};
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyDGfFcoKhDAILb8ii9PhrJVbgrrwbD6tes",
+    authDomain: "swiftuigumieniak.firebaseapp.com",
+    databaseURL: "https://swiftuigumieniak.firebaseio.com",
+    projectId: "swiftuigumieniak",
+    storageBucket: "swiftuigumieniak.appspot.com",
+    messagingSenderId: "507578485948",
+    appId: "1:507578485948:web:42a195351da3a2b37654b9",
+    measurementId: "G-33RGRJ9VB5"
+  };
+
     // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
 var db = firebase.firestore();
 
-function usun(){
-  var yesterday = firebase.firestore.Timestamp.now();
-  yesterday.seconds = yesterday.seconds - (24 * 60 * 60);
-  console.log("Test");
-  db.collection("messages").where("date",">",yesterday)
+function usun(category,deleteDate){
+    db.collection("Categories").doc(category).collection("Reports")
+    .where("Date","<",deleteDate)
+      .get().then(function(querySnapshote) {
+      querySnapshote.forEach(element => {
+        element.ref.delete();
+      });
+    })
+}
+
+function deleteService() {
+  var localThreatenDate = firebase.firestore.Timestamp.now();
+  localThreatenDate.seconds = localThreatenDate.seconds - (1 * 60 * 60);
+  
+  var roadAccidentDate = firebase.firestore.Timestamp.now();
+  roadAccidentDate.seconds = roadAccidentDate.seconds - (6 * 60 * 60);
+
+  var weatherDate = firebase.firestore.Timestamp.now();
+  weatherDate.seconds = weatherDate.seconds - (12 * 60 * 60);
+
+  usun("LocalThreaten",localThreatenDate);
+  usun("RoadAccident",roadAccidentDate);
+  usun("Weather",weatherDate);
+}
+
+function wypisz(category, deleteDate){
+  db.collection("Categories").doc(category).collection("Reports")
+    .where("Date",">",deleteDate)
       .get().then(function(querySnapshote) {
         querySnapshote.forEach(function(doc) {
           console.log(doc.id," => ",doc.data());
@@ -27,11 +52,4 @@ function usun(){
   .catch(function(error) {
         console.log("Error getting documents: ", error);
   });
-
-  db.collection("messages").where("date","<",yesterday)
-    .get().then(function(querySnapshote) {
-      querySnapshote.forEach(element => {
-        element.ref.delete();
-      });
-    })
 }
