19303b56d440303a36c458b245449f6af212827b
Grzegorz Gumieniak <kontaktg.gumieniak@gmail.com>
2020-09-23 13:27:49 +0200

    Dodanie widoku menu

---
+++ ProjectXKG/View/MenuView.swift
@@ -0,0 +1,18 @@
+//
+//  MenuView.swift
+//  ProjectXKG
+//
+//  Created by Grzegorz Gumieniak on 23/09/2020.
+//  Copyright © 2020 Grzegorz Gumieniak. All rights reserved.
+//
+
+import SwiftUI
+
+struct MenuView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+    
+    
+    
+}
